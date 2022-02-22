1d92b72e500d1536910bbb9a025995d90240dce1
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-02-22 15:12:20 +0530

    added history component

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './history.css'
+import Delete from '../../assets/icon-cross.svg'
 
 const History = () => {
   return (
-    <div>History</div>
+    <div className='history'>
+      <h2 className='history-title'>History</h2>
+
+      <div className='transaction'>
+        <p className='transaction-text'>Salary of month Feb</p>
+        <div className='transaction-delete'>
+          <p className='transaction-amount'>$1000</p>
+          <button className='delete-btn'>
+            <img src={Delete} alt='delete icon' className='delete-image'/>
+          </button>
+        </div>
+      </div>
+      <div className='transaction'>
+        <p className='transaction-text'>Salary of month Feb</p>
+        <div className='transaction-delete'>
+          <p className='transaction-amount'>$1000</p>
+          <button className='delete-btn'>
+            <img src={Delete} alt='delete icon' className='delete-image'/>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
